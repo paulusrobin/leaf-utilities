baa9123347c1b75cf01b1fda1e951fe0a9b86ba8
Paulus Robin <paulusrobin@Pauluss-MacBook-Pro.local>
2022-02-24 16:36:00 +0700

    wip kafka

---
+++ messageQueue/kafka/consumer.go
@@ -0,0 +1,197 @@
+package leafKafka
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	"github.com/google/uuid"
+	leafHeader "github.com/paulusrobin/leaf-utilities/common/header"
+	leafMQ "github.com/paulusrobin/leaf-utilities/messageQueue/messageQueue"
+	"sync"
+)
+
+type (
+	Strategy string
+	consumer struct {
+		mu          *sync.Mutex
+		topics      map[string]leafMQ.Dispatcher
+		ready       chan bool
+		listening   bool
+		option      option
+		middlewares []leafMQ.MiddlewareFunc
+		//slackNotification taniSlack.Integration
+	}
+	messageShown struct {
+		ID         string            `json:"id"`
+		Ordering   string            `json:"ordering"`
+		Data       interface{}       `json:"data"`
+		Attributes map[string]string `json:"attributes"`
+	}
+)
+
+var (
+	maskedAttributes = map[string]string{
+		"authorization": `***token***`,
+	}
+)
+
+func (c *consumer) Setup(session sarama.ConsumerGroupSession) error {
+	if c.mu == nil {
+		c.mu = &sync.Mutex{}
+	}
+
+	if c.topics == nil {
+		c.topics = make(map[string]leafMQ.Dispatcher)
+	}
+
+	c.option.logger.StandardLogger().Info("Start Listening")
+	c.listening = true
+	return nil
+}
+
+func (c *consumer) Cleanup(session sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (c *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/master/consumer_group.go#L27-L29
+	for message := range claim.Messages() {
+		dispatcher := c.topics[message.Topic]
+
+		if message == nil {
+			continue
+		}
+
+		if dispatcher == nil {
+			continue
+		}
+
+		c.processMessage(message.Topic, session, message, dispatcher)
+	}
+
+	return nil
+}
+
+func (c *consumer) getMessage(msg sarama.ConsumerMessage) leafMQ.Message {
+	attributes := make(map[string]string)
+	for _, attr := range msg.Headers {
+		attributes[string(attr.Key)] = string(attr.Value)
+	}
+
+	msgID, found := attributes[leafHeader.MessagingID]
+	if !found {
+		msgID = uuid.New().String()
+	}
+
+	mqMessage := leafMQ.Message{
+		Ordering:   string(msg.Key),
+		Data:       msg.Value,
+		Attributes: attributes,
+	}
+	mqMessage.SetID(msgID)
+	return mqMessage
+}
+
+func (c *consumer) processMessage(topic string, session sarama.ConsumerGroupSession, msg *sarama.ConsumerMessage, dispatcher leafMQ.Dispatcher) {
+	messageData := c.getMessage(*msg)
+	traceID := messageData.Attributes[leafHeader.MessagingTraceID]
+	msgType := messageData.Attributes["message"]
+
+	var err error
+	var message = leafMQ.DispatchDTO{
+		Type:      leafMQ.Handle,
+		Source:    fmt.Sprintf("Kafka - %s", topic),
+		RequestID: traceID,
+		MsgType:   msgType,
+		Msg:       messageData,
+		Log:       c.option.logger,
+	}
+	for i := 0; i <= c.option.consumerRetryMax; i++ {
+		message.Err = nil
+		if err = dispatcher.Dispatch(message, c.middlewares...); err == nil {
+			session.MarkMessage(msg, "")
+			return
+		} else {
+			c.option.logger.StandardLogger().Error("error on dispatch message from kafka: ", err.Error())
+			message.Err = err
+		}
+	}
+
+	errMessage := leafMQ.DispatchDTO{
+		Type:      leafMQ.Error,
+		Source:    fmt.Sprintf("Kafka - %s", topic),
+		RequestID: traceID,
+		MsgType:   msgType,
+		Msg:       messageData,
+		Log:       c.option.logger,
+		Err:       err,
+	}
+	_ = dispatcher.Dispatch(errMessage, c.middlewares...)
+	//c.pushNotificationToSlack(message)
+	session.MarkMessage(msg, "")
+}
+
+//func (c *consumer) pushNotificationToSlack(msg leafMQ.DispatchDTO) {
+//	if !c.option.slackNotification.Active || c.slackNotification == nil {
+//		return
+//	}
+//
+//	for key := range msg.Msg.Attributes {
+//		if val, ok := maskedAttributes[key]; ok {
+//			msg.Msg.Attributes[key] = val
+//		}
+//	}
+//
+//	msgShown := messageShown{
+//		ID:         msg.Msg.GetID(),
+//		Ordering:   msg.Msg.Ordering,
+//		Data:       string(msg.Msg.Data),
+//		Attributes: msg.Msg.Attributes,
+//	}
+//
+//	var msgData map[string]interface{}
+//	if err := json.Unmarshal(msg.Msg.Data, &msgData); err == nil {
+//		msgShown.Data = msgData
+//	}
+//
+//	jsonByte, _ := json.MarshalIndent(msgShown, "", "  ")
+//	messageBody, err := taniSlack.NewMessage(
+//		taniSlack.WithBlock(taniSlack.Block{
+//			Type: "header",
+//			Text: taniSlack.Text{
+//				Type: "plain_text",
+//				Text: fmt.Sprintf("Error on consuming Kafka Message: [%s]", msg.RequestID),
+//			},
+//		}),
+//		taniSlack.WithBlock(taniSlack.Block{
+//			Type: "section",
+//			Text: taniSlack.Text{
+//				Type: "mrkdwn",
+//				Text: fmt.Sprintf("<!channel> error on processing this message on %s", msg.Source),
+//			},
+//		}),
+//		taniSlack.WithBlock(taniSlack.Block{
+//			Type: "section",
+//			Text: taniSlack.Text{
+//				Type: "mrkdwn",
+//				Text: fmt.Sprintf("with error: ```%s```", msg.Err.Error()),
+//			},
+//		}),
+//		taniSlack.WithBlock(taniSlack.Block{
+//			Type: "section",
+//			Text: taniSlack.Text{
+//				Type: "mrkdwn",
+//				Text: fmt.Sprintf("with params: ```%s```", string(jsonByte)),
+//			},
+//		}),
+//	)
+//	if err != nil {
+//		c.option.logger.StandardLogger().Errorf("Error on create push notification message to slack: %+v", err.Error())
+//	}
+//
+//	if err := c.slackNotification.Push(context.Background(), messageBody); err != nil {
+//		c.option.logger.StandardLogger().Errorf("Error on push notification to slack: %+v", err.Error())
+//	}
+//}
---
+++ messageQueue/kafka/option.go
@@ -0,0 +1,262 @@
+package leafKafka
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	leafLogger "github.com/paulusrobin/leaf-utilities/logger/logger"
+	leafZap "github.com/paulusrobin/leaf-utilities/logger/zap"
+	"time"
+)
+
+const (
+	DefaultConsumerWorker       = 10
+	DefaultConsumerRetryMax     = 3
+	DefaultConsumerRetryBackoff = 2 * time.Second
+	DefaultFailedDeadline       = 60 * time.Second
+	DefaultStrategy             = "BalanceStrategyRoundRobin"
+	DefaultHeartbeat            = 3
+	DefaultProducerMaxBytes     = 1000000
+	DefaultProducerRetryMax     = 3
+	DefaultProducerRetryBackoff = 100
+	BalanceStrategySticky       = "BalanceStrategySticky"
+	BalanceStrategyRoundRobin   = "BalanceStrategyRoundRobin"
+	BalanceStrategyRange        = "BalanceStrategyRange"
+)
+
+type (
+	Option interface {
+		Apply(o *option)
+	}
+	option struct {
+		host                 []string
+		clientID             string
+		consumerWorker       int
+		consumerGroup        string
+		consumerRetryMax     int
+		consumerRetryBackoff time.Duration
+		strategy             Strategy
+		heartbeat            int
+		producerMaxBytes     int
+		producerRetryMax     int
+		producerRetryBackOff time.Duration
+		kafkaVersion         string
+		logger               leafLogger.Logger
+		withoutProducer      bool
+		withoutConsumer      bool
+		slackNotification    SlackNotification
+	}
+	SlackNotification struct {
+		Active  bool
+		Hook    string
+		Timeout time.Duration
+	}
+)
+
+func defaultOption() option {
+	return option{
+		host:                 make([]string, 0),
+		consumerWorker:       DefaultConsumerWorker,
+		consumerGroup:        "",
+		consumerRetryMax:     DefaultConsumerRetryMax,
+		consumerRetryBackoff: DefaultConsumerRetryBackoff,
+		strategy:             DefaultStrategy,
+		heartbeat:            DefaultHeartbeat,
+		producerMaxBytes:     DefaultProducerMaxBytes,
+		producerRetryMax:     DefaultProducerRetryMax,
+		producerRetryBackOff: DefaultProducerRetryBackoff,
+		kafkaVersion:         "",
+		logger:               leafZap.DefaultLog(),
+		withoutProducer:      false,
+		withoutConsumer:      false,
+		slackNotification: SlackNotification{
+			Active:  true,
+			Hook:    "https://hooks.slack.com/services/",
+			Timeout: 5 * time.Second,
+		},
+	}
+}
+
+func validate(option option) error {
+	if len(option.host) < 1 {
+		return fmt.Errorf("invalid kafka host")
+	}
+	if option.consumerGroup == "" {
+		return fmt.Errorf("invalid kafka consumer group")
+	}
+	if option.kafkaVersion == "" {
+		return fmt.Errorf("invalid kafka version")
+	}
+	return nil
+}
+
+func getStrategy(option option) sarama.BalanceStrategy {
+	if option.strategy == BalanceStrategyRange {
+		return sarama.BalanceStrategyRange
+	}
+
+	if option.strategy == BalanceStrategyRoundRobin {
+		return sarama.BalanceStrategyRoundRobin
+	}
+
+	return sarama.BalanceStrategySticky
+}
+
+type withHost []string
+
+func WithHost(host []string) Option {
+	return withHost(host)
+}
+
+func (w withHost) Apply(o *option) {
+	o.host = w
+}
+
+type withClientID string
+
+func WithClientID(clientID string) Option {
+	return withClientID(clientID)
+}
+
+func (w withClientID) Apply(o *option) {
+	o.clientID = string(w)
+}
+
+type withConsumerWorker int
+
+func WithConsumerWorker(worker int) Option {
+	return withConsumerWorker(worker)
+}
+
+func (w withConsumerWorker) Apply(o *option) {
+	o.consumerWorker = int(w)
+}
+
+type withConsumerGroup string
+
+func WithConsumerGroup(group string) Option {
+	return withConsumerGroup(group)
+}
+
+func (w withConsumerGroup) Apply(o *option) {
+	o.consumerGroup = string(w)
+}
+
+type withConsumerRetryMax int
+
+func WithConsumerRetryMax(maxRetry int) Option {
+	return withProducerRetryMax(maxRetry)
+}
+
+func (w withConsumerRetryMax) Apply(o *option) {
+	o.producerRetryMax = int(w)
+}
+
+type withStrategy Strategy
+
+func WithStrategy(strategy Strategy) Option {
+	return withStrategy(strategy)
+}
+
+func (w withStrategy) Apply(o *option) {
+	o.strategy = Strategy(w)
+}
+
+type withHeartbeat int
+
+func WithHeartbeat(heartbeat int) Option {
+	return withHeartbeat(heartbeat)
+}
+
+func (w withHeartbeat) Apply(o *option) {
+	o.heartbeat = int(w)
+}
+
+type withProducerMaxBytes int
+
+func WithProducerMaxBytes(maxBytes int) Option {
+	return withProducerMaxBytes(maxBytes)
+}
+
+func (w withProducerMaxBytes) Apply(o *option) {
+	o.producerMaxBytes = int(w)
+}
+
+type withProducerRetryMax int
+
+func WithProducerRetryMax(maxRetry int) Option {
+	return withProducerRetryMax(maxRetry)
+}
+
+func (w withProducerRetryMax) Apply(o *option) {
+	o.producerRetryMax = int(w)
+}
+
+type withProducerRetryBackOff time.Duration
+
+func WithProducerRetryBackOff(retryBackoff time.Duration) Option {
+	return withProducerRetryBackOff(retryBackoff)
+}
+
+func (w withProducerRetryBackOff) Apply(o *option) {
+	o.producerRetryBackOff = time.Duration(w)
+}
+
+type withKafkaVersion string
+
+func WithKafkaVersion(version string) Option {
+	return withKafkaVersion(version)
+}
+
+func (w withKafkaVersion) Apply(o *option) {
+	o.kafkaVersion = string(w)
+}
+
+type withLog struct{ leafLogger.Logger }
+
+func WithLog(logger leafLogger.Logger) Option {
+	return withLog{logger}
+}
+
+func (w withLog) Apply(o *option) {
+	o.logger = w
+}
+
+type withoutProducer bool
+
+func WithoutProducer() Option {
+	return withoutProducer(true)
+}
+
+func (w withoutProducer) Apply(o *option) {
+	o.withoutProducer = bool(w)
+}
+
+type withoutConsumer bool
+
+func WithoutConsumer() Option {
+	return withoutConsumer(true)
+}
+
+func (w withoutConsumer) Apply(o *option) {
+	o.withoutConsumer = bool(w)
+}
+
+type withSlackNotification SlackNotification
+
+func (w withSlackNotification) Apply(o *option) {
+	o.slackNotification = SlackNotification(w)
+}
+
+func WithSlackNotification(notification SlackNotification) Option {
+	return withSlackNotification(notification)
+}
+
+type withoutSlackNotification bool
+
+func (w withoutSlackNotification) Apply(o *option) {
+	o.slackNotification.Active = false
+}
+
+func WithoutSlackNotification() Option {
+	return withoutSlackNotification(true)
+}
