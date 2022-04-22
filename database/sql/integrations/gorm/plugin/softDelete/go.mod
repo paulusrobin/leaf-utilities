cbdbd676731a5fef2dd94652170085b9b16a7512
enricodg <enrico.daniswaragiovanni@gmail.com>
2022-04-22 16:59:01 +0700

    add soft delete plugin implementation

---
+++ database/sql/integrations/gorm/plugin/softDelete/go.mod
@@ -0,0 +1,19 @@
+module github.com/paulusrobin/leaf-utilities/database/sql/integrations/gorm/plugin/softDelete
+
+go 1.18
+
+require (
+	github.com/paulusrobin/leaf-utilities/mandatory v0.0.0-20220420081700-6d04b27d3e05
+	gorm.io/gorm v1.23.0
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/paulusrobin/leaf-utilities/encoding/json v0.0.0-20220413144204-972ab9e3b19d // indirect
+	github.com/ua-parser/uap-go v0.0.0-20211112212520-00c877edfe0f // indirect
+	gopkg.in/yaml.v2 v2.2.1 // indirect
+)
---
+++ database/sql/integrations/gorm/plugin/softDelete/soft_delete.go
@@ -0,0 +1,192 @@
+package leafGormSoftDelete
+
+import (
+	leafMandatory "github.com/paulusrobin/leaf-utilities/mandatory"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"gorm.io/gorm/schema"
+	"reflect"
+)
+
+type DeletedAt uint
+
+func (DeletedAt) QueryClauses(f *schema.Field) []clause.Interface {
+	return []clause.Interface{SoftDeleteQueryClause{Field: f}}
+}
+
+type SoftDeleteQueryClause struct {
+	Field *schema.Field
+}
+
+func (sd SoftDeleteQueryClause) Name() string {
+	return ""
+}
+
+func (sd SoftDeleteQueryClause) Build(clause.Builder) {
+}
+
+func (sd SoftDeleteQueryClause) MergeClause(*clause.Clause) {
+}
+
+func (sd SoftDeleteQueryClause) ModifyStatement(stmt *gorm.Statement) {
+	if _, ok := stmt.Clauses["soft_delete_enabled"]; !ok && !stmt.Statement.Unscoped {
+		if c, ok := stmt.Clauses["WHERE"]; ok {
+			if where, ok := c.Expression.(clause.Where); ok && len(where.Exprs) >= 1 {
+				for _, expr := range where.Exprs {
+					if orCond, ok := expr.(clause.OrConditions); ok && len(orCond.Exprs) == 1 {
+						where.Exprs = []clause.Expression{clause.And(where.Exprs...)}
+						c.Expression = where
+						stmt.Clauses["WHERE"] = c
+						break
+					}
+				}
+			}
+		}
+
+		if sd.Field.DefaultValue == "null" {
+			stmt.AddClause(clause.Where{Exprs: []clause.Expression{
+				clause.Eq{Column: clause.Column{Table: clause.CurrentTable, Name: sd.Field.DBName}, Value: nil},
+			}})
+		} else {
+			stmt.AddClause(clause.Where{Exprs: []clause.Expression{
+				clause.Eq{Column: clause.Column{Table: clause.CurrentTable, Name: sd.Field.DBName}, Value: 0},
+			}})
+		}
+		stmt.Clauses["soft_delete_enabled"] = clause.Clause{}
+	}
+}
+
+func (DeletedAt) DeleteClauses(f *schema.Field) []clause.Interface {
+	settings := schema.ParseTagSetting(f.TagSettings["SOFTDELETE"], ",")
+	softDeleteClause := SoftDeleteDeleteClause{
+		Field:    f,
+		Flag:     settings["FLAG"] != "",
+		TimeType: schema.UnixSecond,
+	}
+
+	// flag is much more priority
+	if !softDeleteClause.Flag {
+		if settings["NANO"] != "" {
+			softDeleteClause.TimeType = schema.UnixNanosecond
+		} else if settings["MILLI"] != "" {
+			softDeleteClause.TimeType = schema.UnixMillisecond
+		} else {
+			softDeleteClause.TimeType = schema.UnixSecond
+		}
+	}
+
+	if v := settings["DELETEDATFIELD"]; v != "" { // DeletedAtField
+		softDeleteClause.DeleteAtField = f.Schema.LookUpField(v)
+	}
+	if v := settings["DELETEDBYFIELD"]; v != "" { // DeletedByField
+		softDeleteClause.DeleteByField = f.Schema.LookUpField(v)
+	}
+
+	return []clause.Interface{softDeleteClause}
+}
+
+func (DeletedAt) UpdateClauses(f *schema.Field) []clause.Interface {
+	return []clause.Interface{SoftDeleteUpdateClause{Field: f}}
+}
+
+type SoftDeleteUpdateClause struct {
+	Field *schema.Field
+}
+
+func (sd SoftDeleteUpdateClause) Name() string {
+	return ""
+}
+
+func (sd SoftDeleteUpdateClause) Build(clause.Builder) {
+}
+
+func (sd SoftDeleteUpdateClause) MergeClause(*clause.Clause) {
+}
+
+func (sd SoftDeleteUpdateClause) ModifyStatement(stmt *gorm.Statement) {
+	if stmt.SQL.Len() == 0 && !stmt.Statement.Unscoped {
+		if _, ok := stmt.Clauses["WHERE"]; stmt.DB.AllowGlobalUpdate || ok {
+			SoftDeleteQueryClause(sd).ModifyStatement(stmt)
+		}
+	}
+}
+
+type SoftDeleteDeleteClause struct {
+	Field         *schema.Field
+	Flag          bool
+	TimeType      schema.TimeType
+	DeleteAtField *schema.Field
+	DeleteByField *schema.Field
+}
+
+func (sd SoftDeleteDeleteClause) Name() string {
+	return ""
+}
+
+func (sd SoftDeleteDeleteClause) Build(clause.Builder) {
+}
+
+func (sd SoftDeleteDeleteClause) MergeClause(*clause.Clause) {
+}
+
+func (sd SoftDeleteDeleteClause) ModifyStatement(stmt *gorm.Statement) {
+	if stmt.SQL.Len() == 0 && !stmt.Statement.Unscoped {
+		curTime := stmt.DB.NowFunc()
+		set := clause.Set{}
+		if sd.Flag {
+			set = append(set, clause.Assignment{Column: clause.Column{Name: sd.Field.DBName}, Value: 1})
+			stmt.SetColumn(sd.Field.DBName, 1, true)
+
+			if sd.DeleteAtField != nil {
+				set = append(set, clause.Assignment{Column: clause.Column{Name: sd.DeleteAtField.DBName}, Value: curTime.Unix()})
+				stmt.SetColumn(sd.DeleteAtField.DBName, curTime, true)
+			}
+		} else {
+			var curUnix int64 = 0
+			if sd.TimeType == schema.UnixNanosecond {
+				curUnix = curTime.UnixNano()
+			} else if sd.TimeType == schema.UnixMillisecond {
+				curUnix = curTime.UnixNano() / 1e6
+			} else {
+				curUnix = curTime.Unix()
+			}
+			set = append(set, clause.Assignment{Column: clause.Column{Name: sd.Field.DBName}, Value: curUnix})
+			stmt.SetColumn(sd.Field.DBName, curUnix, true)
+		}
+
+		if sd.DeleteByField != nil {
+			mandatory := leafMandatory.FromContext(stmt.Context)
+			set = append(set, clause.Assignment{Column: clause.Column{Name: sd.DeleteByField.DBName}, Value: mandatory.User().Email()})
+			stmt.SetColumn(sd.DeleteByField.DBName, mandatory.User().Email(), true)
+		}
+
+		stmt.AddClause(set)
+
+		if stmt.Schema != nil {
+			_, queryValues := schema.GetIdentityFieldValuesMap(stmt.Context, stmt.ReflectValue, stmt.Schema.PrimaryFields)
+			column, values := schema.ToQueryValues(stmt.Table, stmt.Schema.PrimaryFieldDBNames, queryValues)
+
+			if len(values) > 0 {
+				stmt.AddClause(clause.Where{Exprs: []clause.Expression{clause.IN{Column: column, Values: values}}})
+			}
+
+			if stmt.ReflectValue.CanAddr() && stmt.Dest != stmt.Model && stmt.Model != nil {
+				_, queryValues = schema.GetIdentityFieldValuesMap(stmt.Context, reflect.ValueOf(stmt.Model), stmt.Schema.PrimaryFields)
+				column, values = schema.ToQueryValues(stmt.Table, stmt.Schema.PrimaryFieldDBNames, queryValues)
+
+				if len(values) > 0 {
+					stmt.AddClause(clause.Where{Exprs: []clause.Expression{clause.IN{Column: column, Values: values}}})
+				}
+			}
+		}
+
+		if _, ok := stmt.Clauses["WHERE"]; !stmt.DB.AllowGlobalUpdate && !ok {
+			stmt.DB.AddError(gorm.ErrMissingWhereClause)
+		} else {
+			SoftDeleteQueryClause{Field: sd.Field}.ModifyStatement(stmt)
+		}
+
+		stmt.AddClauseIfNotExists(clause.Update{})
+		stmt.Build(stmt.DB.Callback().Update().Clauses...)
+	}
+}
