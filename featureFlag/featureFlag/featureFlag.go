ec45dbfa4267070e286584038ea136743360e3ed
enricodg <enrico.daniswaragiovanni@gmail.com>
2022-05-21 13:18:45 +0700

    update feature flag interface

--- featureFlag/featureFlag/featureFlag.go
+++ featureFlag/featureFlag/featureFlag.go
@@ -1,60 +1,9 @@
-package featureFlag
-
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+package leafFeatureFlag
 
 type (
 	FeatureFlag interface {
 		Get(key string) interface{}
-	}
-	implementation struct {
-		serviceName string
-		option      option
-		data        map[string]interface{}
-		sync.Mutex
+		GetKeys() []string
+		GetSettings() map[string]interface{}
 	}
 )
-
-func (f *implementation) init() {
-	fmt.Println("init")
-	
-	f.Lock()
-	defer f.Unlock()
-	// call feature flag
-	var data = map[string]interface{}{
-		"listing": "v2",
-	}
-	f.data = data
-}
-
-func (f *implementation) Get(key string) interface{} {
-	f.Lock()
-	defer f.Unlock()
-	return f.data[key]
-}
-
-func New(serviceName string, opts ...Option) (FeatureFlag, error) {
-	impl := &implementation{serviceName: serviceName, option: defaultOption}
-
-	impl.init()
-	if impl.option.fnValidate != nil {
-		if err := impl.option.fnValidate(impl.data); err != nil {
-			return nil, err
-		}
-	}
-
-	if impl.option.periodicallyUpdate.interval > 0 {
-		go func() {
-			for {
-				select {
-				case <-time.Tick(impl.option.periodicallyUpdate.interval):
-					impl.init()
-				}
-			}
-		}()
-	}
-	return impl, nil
-}
