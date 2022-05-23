964d9cbd1487dc0f7807bdf142ba2014981b6a43
enricodg <enrico.daniswaragiovanni@gmail.com>
2022-05-23 12:01:23 +0700

    update feature flag backup interface

--- featureFlag/featureFlag/backup.go
+++ featureFlag/featureFlag/backup.go
@@ -2,7 +2,7 @@ package leafFeatureFlag
 
 type (
 	Backup interface {
-		Get(key string) map[string]interface{}
+		Get(key string) (map[string]interface{}, error)
 		Set(key string, data map[string]interface{}) error
 	}
 )
